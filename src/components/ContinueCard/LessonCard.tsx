0701b8de30551ab9a7c439f251362af3b64ad2fc
Luan Tran <developedbyluan@gmail.com>
2024-12-12 14:13:43 +0700

    fix (ContinueCard/LessonCard): Remove 'unit' prefix from paths

--- src/components/ContinueCard/LessonCard.tsx
+++ src/components/ContinueCard/LessonCard.tsx
@@ -16,7 +16,7 @@ export default function LessonCard({ lesson, isVertical }: LessonCardProps) {
   const router = useRouter();
 
   const handleLessonCardClick = () => {
-    router.push(`/pronunciation/unit${lesson.id}`);
+    router.push(`/pronunciation/${lesson.id}`);
   };
 
   const fromSecondsToMinutesAndSeconds = (seconds: number) => {
