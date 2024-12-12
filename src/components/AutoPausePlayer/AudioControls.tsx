0fdd6c316a0cbf60cd84bf373b1f20984114d768
Luan Tran <developedbyluan@gmail.com>
2024-12-12 14:26:11 +0700

    style (AutoPausePlayer/AudioControls): change Sync button (go to the previous session lyric)

--- src/components/AutoPausePlayer/AudioControls.tsx
+++ src/components/AutoPausePlayer/AudioControls.tsx
@@ -48,13 +48,18 @@ export default function AudioControls({
             animate={{ y: 0, opacity: 0.5 }}
             exit={{ y: -100, opacity: 0 }}
             transition={{ duration: 0.5, ease: "easeInOut" }}
-            className="fixed top-3 left-1 flex items-center gap-7 px-4 py-3 rounded-xl bg-gradient-to-r from-zinc-700 to-zinc-600"
+            className="fixed top-3 left-1 flex items-center gap-2 rounded-xl bg-gradient-to-r from-zinc-700 to-zinc-600"
           >
-            <button aria-label="Hide the AutoPause player" onClick={() => {}}>
+            <button
+              className="pl-4 pr-4 py-3"
+              aria-label="Hide the AutoPause player"
+              onClick={() => {}}
+            >
               <ChevronDown stroke="white" size={32} />
             </button>
             {!isSynced && (
               <button
+                className="pl-4 pr-4 py-3 bg-zinc-700 rounded-tr-xl rounded-br-xl"
                 onClick={() => {
                   onGotoPreviousSessionLine();
                   setIsSynced(true);
