51cd201ef1c770010c3773f73765e3a6e1ba9339
Luan Tran <developedbyluan@gmail.com>
2024-12-15 08:04:15 +0700

    feat (pwa): prevent zoom on iOS device
    - not workig on Android

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
@@ -9,6 +9,14 @@ export const metadata: Metadata = {
   title: "Flashcard for Speaking Comfortably",
   description:
     "Flashcard app for speaking English comfortably and vocabulary memorization",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  minimumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
